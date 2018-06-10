479f951aa95fd15243af92791a55028c98391611
nikramakrishnan <ramakrishnan.nikhil@gmail.com>
2018-06-10 19:33:02 +0530

    Implement site config generator.
    This implements the site config generator for mkdocs.
    It is yet to be integrated and tested with docwriter.
    
    * siteconfig.py: New file.

---
+++ siteconfig.py
@@ -0,0 +1,229 @@
+#
+#  siteconfig.py
+#
+#    Build site configuration and write to mkdocs.yml.
+#
+#  Copyright 2018 by
+#  Nikhil Ramakrishnan.
+#
+#  This file is part of the FreeType project, and may only be used,
+#  modified, and distributed under the terms of the FreeType project
+#  license, LICENSE.TXT.  By continuing to use, modify, or distribute
+#  this file you indicate that you have read the license and
+#  understand and accept it fully.
+
+import sys
+import utils
+try:
+    import yaml
+except ImportError:
+    sys.stderr.write("Error: Could not find module 'pyyaml'. Please run \
+                      'pip install -r requirements.txt' to install.")
+
+# Config file name
+config_filename = "mkdocs.yml"
+
+# Docs directory and site directory
+docs_dir = "markdown"
+site_dir = "site"
+
+# Basic site configuration default values
+site_name = "FreeType API Reference"
+site_description = "API Reference documentation for FreeType"
+site_author = "FreeType Contributors"
+
+# Theme configuration default values
+theme_conf = {}
+theme_conf['name'] = "material"
+theme_conf['logo'] = ""
+theme_conf['language'] = "en"
+
+# Markdown extensions
+md_extensions = '''\
+markdown_extensions:
+  - toc:
+      permalink: true
+  - pymdownx.superfences:
+      disable_indented_code_blocks: true
+      preserve_tabs: true
+'''
+
+# Extra scripts
+extra_scripts = '''\
+extra_css:
+  - 'stylesheets/extra.css'
+
+extra_javascript:
+  - 'javascripts/jquery-3.3.1.min.js'
+  - 'javascripts/extra.js'
+'''
+
+# Other config
+other_config = '''\
+copyright: Copyright 2018 <a href = "http://git.savannah.gnu.org/cgit/freetype/freetype2.git/tree/docs/LICENSE.TXT">The FreeType Project</a>.
+'''
+
+def add_config( yml_string, config_name ):
+    config = None
+    try:
+        config = yaml.safe_load( yml_string )
+    except:
+        sys.stderr.write("WARNING: Malformed '"+ config_name +"' config, ignoring.\n")
+    return config
+
+# Parse all configurations and save as Python objects
+markdown_extensions = add_config( md_extensions, "markdown_extensions" )
+yml_extra           = add_config( extra_scripts, "extra scripts" )
+yml_other           = add_config( other_config, "other" )
+
+
+class Chapter:
+    def __init__( self, title ):
+        self.title = title
+        self.pages = []
+
+    def add_page( self, section_title, filename ):
+        cur_page = {}
+        cur_page[section_title] = filename
+        self.pages.append( cur_page )
+
+    def get_pages( self ):
+        conf = {}
+        conf[self.title] = self.pages
+        return conf
+
+
+class SiteConfig:
+    '''Site configuration generator class
+
+    This class is used to generate site configuration based on
+    supplied and default values.
+    '''
+    def __init__( self ):
+        self.site_config = {}
+        self.pages = []
+        self.chapter = None
+        self.sections = []
+        self.markdown_extensions = []
+
+        global site_name, site_description, site_author
+        global docs_dir, site_dir
+        global theme_conf
+
+        self.site_name = site_name
+        self.site_description = site_description
+        self.site_author = site_author
+        self.docs_dir = docs_dir
+        self.site_dir = site_dir
+        self.theme_conf = theme_conf
+
+    def set_site_info( self, name, description = None, author = None ):
+        '''Set the basic site information'''
+        if name:
+            self.site_name = name
+        else:
+            sys.stderr.write("WARNING: Site name not specified, reverting to default.\n")
+
+        if description:
+            self.site_description = description
+        if author:
+            self.site_author = author
+
+    def add_single_page( self, section_title, filename ):
+        '''Add a single page to the list of pages'''
+        cur_page = {}
+        cur_page[section_title] = filename
+        self.pages.append( cur_page )
+
+    def add_chapter_page(self, section_title, filename):
+        '''Add a page to a chapter.
+
+        Chapter must be set first using `start_chapter()`
+        '''
+        if self.chapter:
+            self.chapter.add_page( section_title, filename )
+        else:
+            sys.stderr.write("WARNING: Section '"+ section_title + "' added without starting chapter.\n")
+            self.add_single_page( section_title, filename )
+
+    def start_chapter( self, chap ):
+        '''Start a chapter'''
+        if self.chapter:
+            chap_pages = self.chapter.get_pages()
+            self.pages.append( chap_pages )
+
+        self.chapter = Chapter( chap )
+
+    def end_chapter( self ):
+        '''Explicitly end a chapter'''
+        if self.chapter:
+            chap_pages = self.chapter.get_pages()
+            self.pages.append( chap_pages )
+            self.chapter = None
+
+    def build_site_config( self ):
+        '''Add basic Project information to config'''
+        self.site_config['site_name'] = self.site_name
+        if site_description:
+            self.site_config['site_description'] = self.site_description
+        if site_author:
+            self.site_config['site_author'] = self.site_author
+        if docs_dir:
+            self.site_config['docs_dir'] = self.docs_dir
+        if site_dir:
+            self.site_config['site_dir'] = self.site_dir
+
+    def build_theme_config( self ):
+        if theme_conf != {}:
+            self.site_config['theme'] = self.theme_conf
+
+    def build_pages( self ):
+        if self.pages != []:
+            self.site_config['pages'] = self.pages
+
+    def populate_config( self, data ):
+        '''Add a given not None object to site_config'''
+        if data:
+            self.site_config.update( data )
+
+    def write_config( self, name ):
+        '''Write all values in site_config to output stream'''
+        if self.site_config != {}:
+            print( "# " + name )
+            print( yaml.dump( self.site_config ) )
+            self.site_config = {}
+
+    def build_config( self ):
+        '''Build the YAML configuration'''
+        # End chapter if started
+        self.end_chapter()
+
+        # Open yml file
+        output = utils.open_output( config_filename )
+
+        # Build basic site info
+        self.build_site_config()
+        self.write_config("Project information")
+
+        # Build theme configuration
+        self.build_theme_config()
+        self.write_config("Configuration")
+
+        # Build pages
+        self.build_pages()
+        self.write_config("Pages")
+
+        # Add extra CSS and Javascript
+        self.populate_config( yml_extra )
+        self.write_config("Customization")
+
+        # Add Markdown extensions
+        self.populate_config( markdown_extensions )
+        self.write_config("Extensions")
+
+        # Add other options
+        self.populate_config( yml_other )
+        self.write_config("Other Options")
+
+        # Close the file
+        utils.close_output(output)
