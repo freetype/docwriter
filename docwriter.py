f6f9dd07d89fa72bb5f17d563011e0790f2324a1
nikramakrishnan <ramakrishnan.nikhil@gmail.com>
2018-07-14 19:14:12 +0530

    [docwriter] Use argparse instead of getopt.
    This has a lot of advantages:
    
    * No need to maintain a usage() function, argparse populates help text
    automatically.
    * Easier to add arguments, and more readable code.
    * Allows adding mutually exclusive arguments easily (--verbose and
    --quiet for instance).
    
    This commit also uses the code formatter 'black' with line length = 80,
    as a test.

--- docwriter.py
+++ docwriter.py
@@ -18,7 +18,7 @@
 # the API Reference of the FreeType font rendering engine by converting
 # in-source comments into structured HTML.
 #
-# This new version is capable of outputting XML/Markdown data as well as 
+# This new version is capable of outputting XML/Markdown data as well as
 # accepting more liberal formatting options.  It also uses regular expression
 # matching and substitution to speed up operation significantly.
 #
@@ -30,7 +30,7 @@ documentation.
 
 from __future__ import print_function
 
-import getopt
+import argparse
 import logging
 import sys
 
@@ -40,112 +40,115 @@ import sources
 import tomarkdown
 import utils
 
-logger    = logging.getLogger()
+logger = logging.getLogger()
 log_level = logging.INFO
 
-def  usage():
-    print( "\nDocWriter Usage information\n" )
-    print( "  docwriter [options] file1 [file2 ...]\n" )
-    print( "Note: Wildcard names are supported\n" )
-    print( "using the following options:\n" )
-    print( "  -h : print this page" )
-    print( "  -t : set project title, as in '-t \"My Project\"'" )
-    print( "  -o : set output directory, as in '-o mydir'" )
-    print( "  -p : set documentation prefix, as in '-p ft2'" )
-    print( "  -q : run quietly, show only errors" )
-    print( "  -v : verbose" )
-    print( "" )
-    print( "  --title  : same as -t, as in '--title=\"My Project\"'" )
-    print( "  --output : same as -o, as in '--output=mydir'" )
-    print( "  --prefix : same as -p, as in '--prefix=ft2'" )
-    print( "  --quiet  : same as -q" )
-    print( "  --verbose: same as -v" )
 
-def  setup_logger( level=logging.INFO ):
+def setup_logger(level=logging.INFO):
     """Setup the logger."""
     logger.propagate = False
     stream = logging.StreamHandler()
-    log_format = logging.Formatter( "%(levelname)-7s -  %(message)s" )
-    stream.setFormatter( log_format )
-    logger.addHandler( stream )
+    log_format = logging.Formatter("%(levelname)-7s -  %(message)s")
+    stream.setFormatter(log_format)
+    logger.addHandler(stream)
 
-    logger.setLevel( level )
+    logger.setLevel(level)
 
-def  main( argv ):
+
+def main(argv):
     """Main program loop."""
 
     global output_dir
     global log_level
 
-    try:
-        opts, args = getopt.getopt( argv[1:],
-                                    "hqvt:o:p:",
-                                    ["help", "quiet", "verbose",
-                                     "title=", "output=", "prefix="] )
-    except getopt.GetoptError:
-        usage()
-        sys.exit( 2 )
-
-    if args == []:
-        usage()
-        sys.exit( 1 )
+    parser = argparse.ArgumentParser(description="DocWriter Usage information")
+    parser.add_argument(
+        "files",
+        nargs="+",
+        help="list of source files to parse, wildcards are allowed",
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        metavar="T",
+        help="set project title, as in '-t \"My Project\"'",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="DIR",
+        required=True,
+        help="set output directory, as in '-o mydir'",
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        metavar="PRE",
+        help="set documentation prefix, as in '-p ft2'",
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-q",
+        "--quiet",
+        help="run quietly, show only errors",
+        action="store_true",
+    )
+    group.add_argument(
+        "-v", "--verbose", help="increase output verbosity", action="store_true"
+    )
+    args = parser.parse_args()
 
     # process options
-    project_title  = "Project"
+    project_title = "Project"
     project_prefix = None
-    output_dir     = None
+    output_dir = None
 
-    for opt in opts:
-        if opt[0] in ( "-h", "--help" ):
-            usage()
-            sys.exit( 0 )
+    if args.title:
+        project_title = args.title
 
-        if opt[0] in ( "-t", "--title" ):
-            project_title = opt[1]
+    if args.output:
+        utils.output_dir = args.output
 
-        if opt[0] in ( "-o", "--output" ):
-            utils.output_dir = opt[1]
+    if args.prefix:
+        project_prefix = args.prefix
 
-        if opt[0] in ( "-p", "--prefix" ):
-            project_prefix = opt[1]
+    if args.quiet:
+        log_level = logging.ERROR
 
-        if opt[0] in ( "-q", "--quiet" ):
-            log_level = logging.ERROR
-
-        if opt[0] in ( "-v", "--verbose" ):
-            log_level = logging.DEBUG
+    if args.verbose:
+        log_level = logging.DEBUG
 
     # set up the logger
-    setup_logger( level = log_level )
-    log = logging.getLogger('docwriter')
+    setup_logger(level=log_level)
+    log = logging.getLogger("docwriter")
 
     # check all packages
     status = check.check()
     if status != 0:
-        sys.exit( 3 )
+        sys.exit(3)
 
     utils.check_output()
 
     # create context and processor
-    source_processor  = sources.SourceProcessor()
+    source_processor = sources.SourceProcessor()
     content_processor = content.ContentProcessor()
 
     # retrieve the list of files to process
-    file_list = utils.make_file_list( args )
+    file_list = utils.make_file_list(args.files)
     for filename in file_list:
-        source_processor.parse_file( filename )
-        content_processor.parse_sources( source_processor )
+        source_processor.parse_file(filename)
+        content_processor.parse_sources(source_processor)
 
     # process sections
     content_processor.finish()
 
     # clean up directory
-    log.info( "Cleaning output directory" )
+    log.info("Cleaning output directory")
     utils.clean_markdown_dir()
 
-    formatter = tomarkdown.MdFormatter( content_processor,
-                                        project_title,
-                                        project_prefix )
+    formatter = tomarkdown.MdFormatter(
+        content_processor, project_title, project_prefix
+    )
 
     # build the docs
     utils.build_message()
@@ -156,7 +159,7 @@ def  main( argv ):
 
 
 # if called from the command line
-if __name__ == '__main__':
-    main( sys.argv )
+if __name__ == "__main__":
+    main(sys.argv)
 
 # eof
